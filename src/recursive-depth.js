0e0cad66898604b4c8e0581f252a9df8316b20ce
VZaleski <70795078+VZaleski@users.noreply.github.com>
2020-11-01 21:35:17 +0300

    add: carbon, recursive - depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,14 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+calculateDepth(arr) {
+	if(arr instanceof Array) {
+		let maximal = 0;
+		for(let i = 0; i< arr.length;i++) {
+			let cur = this.calculateDepth.call(this,arr[i]);
+			if(maximal < cur) {
+				maximal = cur;
+			}
+		}
+			return 1 + maximal;
+		} else {
+			return 0;
+		}
+	}
 };
-/*function calculateDepth( arr ) {
-  if (Array.isArray(arr)) return 1 + Math.max(...arr.map(t => calculateDepth(t)))
-  else return 0
-}
-console.log(calculateDepth([1,2,[3,4,[5,6],7,[8,[9,91]],10],11,12]))
-console.log(calculateDepth([1,[1]]))*/
+
+
