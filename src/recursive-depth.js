94d4694ae00ec218a99139be6994811da4186a58
VZaleski <70795078+VZaleski@users.noreply.github.com>
2020-11-01 19:59:36 +0300

    fix: recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,38 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-/*module.exports = class DepthCalculator {
+module.exports = class DepthCalculator {
   calculateDepth( arr ) {
-      if (Array.isArray(arr)){
-        return 1 + Math.max(...arr.map(t => getArrayDepth(t)));
-      } 
-      else return 0;
-  }
+    throw new CustomError('Not implemented');
+    // remove line with error and write your code here
 };
-console.log(DepthCalculator.calculateDepth([123]));*/
-/*class DepthCalculator {
-  calculateDepth( arr ) {
-      if (Array.isArray(arr)){
-        return 1 + Math.max(...arr.map(t => calculateDepth(t)));
-      } 
-      else return 0;
-  }
-};
-const depthCalc = new DepthCalculator();
-depthCalc.calculateDepth([1, 2, 3, 4, 5]);
-console.log(depthCalc);*/
-
-class DepthCalculator{
-  calculateDepth( arr ) {
-    if (Array.isArray(arr)){
-      return 1 + Math.max(...arr.map(t => calculateDepth(t)));
-    } else {
-      return 0;
-    }
-  }
-}
-const depth = new DepthCalculator();
-depth.calculateDepth([1,2,[3,4,[5,6],7,[8,[9,91]],10],11,12]);
-console.log(depth);
 /*function calculateDepth( arr ) {
   if (Array.isArray(arr)) return 1 + Math.max(...arr.map(t => calculateDepth(t)))
   else return 0
